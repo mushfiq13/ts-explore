bb043850fb5c2fb7e4fdb0f986d1f00f19b7ea5f
Mushfiqur <mushfiqurrahman4513@gmail.com>
2023-11-05 18:49:21 +0600

    Update tsconfig. Setup WebPack. Build and Run the project.

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.ts",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist/js"),
+  },
+  module: {
+    rules: [
+      {
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  devtool: "source-map",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 9000,
+  },
+};
